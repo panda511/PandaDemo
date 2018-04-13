13cdd1800bb1abfbb2dda8d7866ae54f3159264b
sunjian <mayn@DESKTOP-Q08CDEK>
2018-04-13 14:00:16 +0800

    zzz

---
+++ PandaDemo/ModelVerify/Controllers/PersonController.cs
@@ -0,0 +1,18 @@
+﻿using ModelVerify.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ModelVerify.Controllers
+{
+    public class PersonController : ApiController
+    {
+        public Person GetData(Person p)
+        {
+            return p;
+        }
+    }
+}
