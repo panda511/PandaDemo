b29a49b0dbcf4bc560c36e4a668f521a7da3d7fe
sunjian <mayn@DESKTOP-Q08CDEK>
2018-04-13 10:14:49 +0800

    k

--- PandaDemo/ADO.NET/Program.cs
+++ PandaDemo/ADO.NET/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,23 @@ namespace ADO.NET
     {
         static void Main(string[] args)
         {
-             
+            string str = @"data source=(local);database=BaseCardDB;user id=sa;password=123456;";
+            using (SqlConnection connection = new SqlConnection(str))
+            {
+                string sql = "select * from product";
+
+                SqlCommand command = new SqlCommand(sql, connection);
+                connection.Open();
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader["ProductName"].ToString().Trim());
+                    }
+                }
+            }
+            Console.WriteLine("ok");
+            Console.ReadLine();
         }
     }
 }
