bb0617c09f8a38a9a8f08fe27971d8e8ea901c17
sunjian <mayn@DESKTOP-Q08CDEK>
2018-04-13 10:44:59 +0800

    zzz

--- PandaDemo/FileReadWrite/Program.cs
+++ PandaDemo/FileReadWrite/Program.cs
@@ -18,5 +18,17 @@ namespace FileReadWrite
                 Console.WriteLine(line);
             }
         }
+
+        private static void Write(string message)
+        {
+            string filePath = "shipping_" + DateTime.Now.ToString("yyyy_MM_dd") + ".txt";
+            FileStream fileStream = new FileStream(filePath, FileMode.Append);
+            StreamWriter streamWriter = new StreamWriter(fileStream, Encoding.Default);
+
+            streamWriter.WriteLine(message);
+
+            streamWriter.Close();
+            fileStream.Close();
+        }
     }
 }
