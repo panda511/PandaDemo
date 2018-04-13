3cdcf600d6817aa3d0cc7d460ea7123b0a2edcef
sunjian <mayn@DESKTOP-Q08CDEK>
2018-04-13 10:43:13 +0800

    zzz

---
+++ PandaDemo/DateTime/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DateTime2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt1 = new DateTime(2013, 5, 14);
+            DateTime dt2 = new DateTime(2013, 5, 16);
+            TimeSpan ts = dt2 - dt1; 
+        }
+    }
+}
