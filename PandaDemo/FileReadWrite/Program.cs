8a000bcfbf409f0a8d6b7a63205bd6e3db06e66e
sunjian <mayn@DESKTOP-Q08CDEK>
2018-04-13 10:34:29 +0800

    zzz

---
+++ PandaDemo/FileReadWrite/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileReadWrite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"E:\ff.txt");
+            string line = string.Empty;
+            int count = 0;
+            while ((line = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+        }
+    }
+}
