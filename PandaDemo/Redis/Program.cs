6f91996b1e1a4269e35e61670e45f7ee2c82c5a6
sunjian <mayn@DESKTOP-Q08CDEK>
2018-04-16 11:48:06 +0800

    zzz

---
+++ PandaDemo/Redis/Program.cs
@@ -0,0 +1,111 @@
+﻿using Newtonsoft.Json;
+using RedisDemo;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Redis
+{
+    class Program
+    {
+        static void Main66(string[] args)
+        {
+            Person p = new Person();
+            p.Id = 1;
+            p.Name = "李四2";
+            p.Age = 22;
+            List<Person> list = new List<Person>();
+            list.Add(p);
+
+            ConfigurationOptions opt = ConfigurationOptions.Parse("127.0.0.1:63790");
+            opt.Password = "666";
+            opt.AllowAdmin = true;
+            opt.ConnectTimeout = 5000;
+            opt.SyncTimeout = 5000;
+            opt.DefaultDatabase = 5;
+
+            try
+            {
+                ConnectionMultiplexer c = ConnectionMultiplexer.Connect(opt); //"127.0.0.1:6379,allowadmin=true"
+                IDatabase db = c.GetDatabase();
+
+                db.StringSet("abc2:name", JsonConvert.SerializeObject(list), TimeSpan.FromMinutes(5)); //设置过期时间
+
+                List<Person> list2 = JsonConvert.DeserializeObject<List<Person>>(db.StringGet("abc2:name"));
+
+                Console.WriteLine(list2[0].Name);
+            }
+            catch
+            {
+                Console.WriteLine("error");
+            }
+            Console.Read();
+        }
+
+        static void Main22(string[] args)
+        {
+            RedisHelper h = new RedisHelper();
+            //h.StringSet("name", "tom");
+            string s = h.StringGet("name");
+            Console.WriteLine(s);
+        }
+
+        static void Main(string[] args)
+        {
+            var redis = ConnectionMultiplexer.Connect("127.0.0.1:6379,allowAdmin = true");
+            redis.GetDatabase().ScriptEvaluate(LuaScript.Prepare(
+                //Redis的keys模糊查询：
+                " local ks = redis.call('KEYS', @keypattern) " + //local ks为定义一个局部变量，其中用于存储获取到的keys
+                " for i=1,#ks,5000 do " +    //#ks为ks集合的个数, 语句的意思： for(int i = 1; i <= ks.Count; i+=5000)
+                "     redis.call('del', unpack(ks, i, math.min(i+4999, #ks))) " + //Lua集合索引值从1为起始，unpack为解包，获取ks集合中的数据，每次5000，然后执行删除
+                " end " +
+                " return true "
+                ),
+                new { keypattern = "mykey*" }
+            );
+
+            //redis0:6379,redis1:6380,keepAlive=180,version=2.8.8,$CLIENT=,$CLUSTER=,$CONFIG=,$ECHO=,$INFO=,$PING=
+            ConfigurationOptions config = new ConfigurationOptions
+            {
+                EndPoints = {
+                    { "redis0", 6379 },
+                    { "redis1", 6380 }
+                },
+                CommandMap = CommandMap.Create(
+                    // 排除几个命令
+                    new HashSet<string> { "INFO", "CONFIG", "CLUSTER", "PING", "ECHO", "CLIENT" }, available: false
+                ),
+                KeepAlive = 180,
+                DefaultVersion = new Version(2, 8, 8),
+                Password = "changeme"
+            };
+
+            string someKey = redis.GetDatabase().KeyRandom();
+            redis.GetDatabase().KeyDelete("key");
+
+            //redis.GetDatabase().ke
+
+            // 取得目标服务器
+            var server = redis.GetServer("someServer");
+
+            // 在索引为0的数据库中展示出所有的key，这个key的名字必须匹配 *foo*
+            foreach (var key in server.Keys(pattern: "*foo*"))
+            {
+                Console.WriteLine(key);
+            }
+
+            // 从索引为0的数据库中清除所有的key
+            server.FlushDatabase();
+        }
+
+        public class Person
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+    }
+}
---
+++ PandaDemo/Redis/RedisConnectionHelp.cs
@@ -0,0 +1,422 @@
+﻿using Newtonsoft.Json;
+using StackExchange.Redis;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisDemo
+{
+    /// <summary>
+    /// ConnectionMultiplexer对象管理帮助类
+    /// </summary>
+    public static class RedisConnectionHelp
+    {
+        //系统自定义Key前缀
+        public static readonly string SysCustomKey = "abc:";
+
+        private static readonly string RedisConnectionString = "127.0.0.1:6379,allowadmin=true"; 
+
+        private static readonly object Locker = new object();
+        private static ConnectionMultiplexer _instance;
+        private static readonly ConcurrentDictionary<string, ConnectionMultiplexer> ConnectionCache = new ConcurrentDictionary<string, ConnectionMultiplexer>();
+
+        /// <summary>
+        /// 单例获取
+        /// </summary>
+        public static ConnectionMultiplexer Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    lock (Locker)
+                    {
+                        if (_instance == null || !_instance.IsConnected)
+                        {
+                            _instance = GetManager();
+                        }
+                    }
+                }
+                return _instance;
+            }
+        }
+
+        /// <summary>
+        /// 缓存获取
+        /// </summary>
+        /// <param name="connectionString"></param>
+        /// <returns></returns>
+        public static ConnectionMultiplexer GetConnectionMultiplexer(string connectionString)
+        {
+            if (!ConnectionCache.ContainsKey(connectionString))
+            {
+                ConnectionCache[connectionString] = GetManager(connectionString);
+            }
+            return ConnectionCache[connectionString];
+        }
+
+        private static ConnectionMultiplexer GetManager(string connectionString = null)
+        {
+            connectionString = connectionString ?? RedisConnectionString;
+            var connect = ConnectionMultiplexer.Connect(connectionString);
+
+            //注册如下事件
+            connect.ConnectionFailed += MuxerConnectionFailed;
+            connect.ConnectionRestored += MuxerConnectionRestored;
+            connect.ErrorMessage += MuxerErrorMessage;
+            connect.ConfigurationChanged += MuxerConfigurationChanged;
+            connect.HashSlotMoved += MuxerHashSlotMoved;
+            connect.InternalError += MuxerInternalError;
+
+            return connect;
+        }
+
+        #region 事件
+
+        /// <summary>
+        /// 配置更改时
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void MuxerConfigurationChanged(object sender, EndPointEventArgs e)
+        {
+            Console.WriteLine("Configuration changed: " + e.EndPoint);
+        }
+
+        /// <summary>
+        /// 发生错误时
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void MuxerErrorMessage(object sender, RedisErrorEventArgs e)
+        {
+            Console.WriteLine("ErrorMessage: " + e.Message);
+        }
+
+        /// <summary>
+        /// 重新建立连接之前的错误
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void MuxerConnectionRestored(object sender, ConnectionFailedEventArgs e)
+        {
+            Console.WriteLine("ConnectionRestored: " + e.EndPoint);
+        }
+
+        /// <summary>
+        /// 连接失败， 如果重新连接成功你将不会收到这个通知
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void MuxerConnectionFailed(object sender, ConnectionFailedEventArgs e)
+        {
+            Console.WriteLine("重新连接：Endpoint failed: " + e.EndPoint + ", " + e.FailureType + (e.Exception == null ? "" : (", " + e.Exception.Message)));
+        }
+
+        /// <summary>
+        /// 更改集群
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void MuxerHashSlotMoved(object sender, HashSlotMovedEventArgs e)
+        {
+            Console.WriteLine("HashSlotMoved:NewEndPoint" + e.NewEndPoint + ", OldEndPoint" + e.OldEndPoint);
+        }
+
+        /// <summary>
+        /// redis类库错误
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private static void MuxerInternalError(object sender, InternalErrorEventArgs e)
+        {
+            Console.WriteLine("InternalError:Message" + e.Exception.Message);
+        }
+
+        #endregion 事件
+    }
+
+
+    public class RedisHelper
+    {
+        private int DbNum { get; }
+        private readonly ConnectionMultiplexer _conn;
+        public string CustomKey;
+
+        #region 构造函数
+
+        public RedisHelper(int dbNum = 0)
+                : this(dbNum, null)
+        {
+        }
+
+        public RedisHelper(int dbNum, string readWriteHosts)
+        {
+            DbNum = dbNum;
+            _conn =
+                string.IsNullOrWhiteSpace(readWriteHosts) ?
+                RedisConnectionHelp.Instance :
+                RedisConnectionHelp.GetConnectionMultiplexer(readWriteHosts);
+        }
+
+
+
+        #endregion 构造函数
+
+        #region 辅助方法
+
+        private string AddSysCustomKey(string oldKey)
+        {
+            var prefixKey = CustomKey ?? RedisConnectionHelp.SysCustomKey;
+            return prefixKey + oldKey;
+        }
+
+        private T Do<T>(Func<IDatabase, T> func)
+        {
+            var database = _conn.GetDatabase(DbNum);
+            return func(database);
+        }
+
+        private string ConvertJson<T>(T value)
+        {
+            string result = value is string ? value.ToString() : JsonConvert.SerializeObject(value);
+            return result;
+        }
+
+        private T ConvertObj<T>(RedisValue value)
+        {
+            return JsonConvert.DeserializeObject<T>(value);
+        }
+
+        private List<T> ConvetList<T>(RedisValue[] values)
+        {
+            List<T> result = new List<T>();
+            foreach (var item in values)
+            {
+                var model = ConvertObj<T>(item);
+                result.Add(model);
+            }
+            return result;
+        }
+
+        private RedisKey[] ConvertRedisKeys(List<string> redisKeys)
+        {
+            return redisKeys.Select(redisKey => (RedisKey)redisKey).ToArray();
+        }
+
+        #endregion 辅助方法
+
+        #region String
+
+        #region 同步方法
+
+        /// <summary>
+        /// 保存单个key value
+        /// </summary>
+        /// <param name="key">Redis Key</param>
+        /// <param name="value">保存的值</param>
+        /// <param name="expiry">过期时间</param>
+        /// <returns></returns>
+        public bool StringSet(string key, string value, TimeSpan? expiry = default(TimeSpan?))
+        {
+            key = AddSysCustomKey(key);
+            return Do(db => db.StringSet(key, value, expiry));
+        }
+
+        /// <summary>
+        /// 保存多个key value
+        /// </summary>
+        /// <param name="keyValues">键值对</param>
+        /// <returns></returns>
+        public bool StringSet(List<KeyValuePair<RedisKey, RedisValue>> keyValues)
+        {
+            List<KeyValuePair<RedisKey, RedisValue>> newkeyValues =
+                keyValues.Select(p => new KeyValuePair<RedisKey, RedisValue>(AddSysCustomKey(p.Key), p.Value)).ToList();
+            return Do(db => db.StringSet(newkeyValues.ToArray()));
+        }
+
+        /// <summary>
+        /// 保存一个对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="obj"></param>
+        /// <param name="expiry"></param>
+        /// <returns></returns>
+        public bool StringSet<T>(string key, T obj, TimeSpan? expiry = default(TimeSpan?))
+        {
+            key = AddSysCustomKey(key);
+            string json = ConvertJson(obj);
+            return Do(db => db.StringSet(key, json, expiry));
+        }
+
+        /// <summary>
+        /// 获取单个key的值
+        /// </summary>
+        /// <param name="key">Redis Key</param>
+        /// <returns></returns>
+        public string StringGet(string key)
+        {
+            key = AddSysCustomKey(key);
+            return Do(db => db.StringGet(key));
+        }
+
+        /// <summary>
+        /// 获取多个Key
+        /// </summary>
+        /// <param name="listKey">Redis Key集合</param>
+        /// <returns></returns>
+        public RedisValue[] StringGet(List<string> listKey)
+        {
+            List<string> newKeys = listKey.Select(AddSysCustomKey).ToList();
+            return Do(db => db.StringGet(ConvertRedisKeys(newKeys)));
+        }
+
+        /// <summary>
+        /// 获取一个key的对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public T StringGet<T>(string key)
+        {
+            key = AddSysCustomKey(key);
+            return Do(db => ConvertObj<T>(db.StringGet(key)));
+        }
+
+        /// <summary>
+        /// 为数字增长val
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="val">可以为负</param>
+        /// <returns>增长后的值</returns>
+        public double StringIncrement(string key, double val = 1)
+        {
+            key = AddSysCustomKey(key);
+            return Do(db => db.StringIncrement(key, val));
+        }
+
+        /// <summary>
+        /// 为数字减少val
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="val">可以为负</param>
+        /// <returns>减少后的值</returns>
+        public double StringDecrement(string key, double val = 1)
+        {
+            key = AddSysCustomKey(key);
+            return Do(db => db.StringDecrement(key, val));
+        }
+
+        #endregion 同步方法
+
+        #region 异步方法
+
+        /// <summary>
+        /// 保存单个key value
+        /// </summary>
+        /// <param name="key">Redis Key</param>
+        /// <param name="value">保存的值</param>
+        /// <param name="expiry">过期时间</param>
+        /// <returns></returns>
+        public async Task<bool> StringSetAsync(string key, string value, TimeSpan? expiry = default(TimeSpan?))
+        {
+            key = AddSysCustomKey(key);
+            return await Do(db => db.StringSetAsync(key, value, expiry));
+        }
+
+        /// <summary>
+        /// 保存多个key value
+        /// </summary>
+        /// <param name="keyValues">键值对</param>
+        /// <returns></returns>
+        public async Task<bool> StringSetAsync(List<KeyValuePair<RedisKey, RedisValue>> keyValues)
+        {
+            List<KeyValuePair<RedisKey, RedisValue>> newkeyValues =
+                keyValues.Select(p => new KeyValuePair<RedisKey, RedisValue>(AddSysCustomKey(p.Key), p.Value)).ToList();
+            return await Do(db => db.StringSetAsync(newkeyValues.ToArray()));
+        }
+
+        /// <summary>
+        /// 保存一个对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="obj"></param>
+        /// <param name="expiry"></param>
+        /// <returns></returns>
+        public async Task<bool> StringSetAsync<T>(string key, T obj, TimeSpan? expiry = default(TimeSpan?))
+        {
+            key = AddSysCustomKey(key);
+            string json = ConvertJson(obj);
+            return await Do(db => db.StringSetAsync(key, json, expiry));
+        }
+
+        /// <summary>
+        /// 获取单个key的值
+        /// </summary>
+        /// <param name="key">Redis Key</param>
+        /// <returns></returns>
+        public async Task<string> StringGetAsync(string key)
+        {
+            key = AddSysCustomKey(key);
+            return await Do(db => db.StringGetAsync(key));
+        }
+
+        /// <summary>
+        /// 获取多个Key
+        /// </summary>
+        /// <param name="listKey">Redis Key集合</param>
+        /// <returns></returns>
+        public async Task<RedisValue[]> StringGetAsync(List<string> listKey)
+        {
+            List<string> newKeys = listKey.Select(AddSysCustomKey).ToList();
+            return await Do(db => db.StringGetAsync(ConvertRedisKeys(newKeys)));
+        }
+
+        /// <summary>
+        /// 获取一个key的对象
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public async Task<T> StringGetAsync<T>(string key)
+        {
+            key = AddSysCustomKey(key);
+            string result = await Do(db => db.StringGetAsync(key));
+            return ConvertObj<T>(result);
+        }
+
+        /// <summary>
+        /// 为数字增长val
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="val">可以为负</param>
+        /// <returns>增长后的值</returns>
+        public async Task<double> StringIncrementAsync(string key, double val = 1)
+        {
+            key = AddSysCustomKey(key);
+            return await Do(db => db.StringIncrementAsync(key, val));
+        }
+
+        /// <summary>
+        /// 为数字减少val
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="val">可以为负</param>
+        /// <returns>减少后的值</returns>
+        public async Task<double> StringDecrementAsync(string key, double val = 1)
+        {
+            key = AddSysCustomKey(key);
+            return await Do(db => db.StringDecrementAsync(key, val));
+        }
+
+        #endregion 异步方法
+
+        #endregion String
+    }
+
+}
