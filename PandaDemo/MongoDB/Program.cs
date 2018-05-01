740426e528f55af67a27d51e7968a7aa11c09ff7
Panda <Panda@Panda.com>
2018-05-01 20:03:08 +0800

    zzz

--- PandaDemo/MongoDB/Program.cs
+++ PandaDemo/MongoDB/Program.cs
@@ -14,7 +14,8 @@ namespace MongoDB
         {
             try
             {
-                string connectionString = "mongodb://127.0.0.1:27017?ServerSelectionTimeout=1";
+                //原生查询
+                //{"CreateDate":{"$gte":new Date("2017-02-26T09:00:00Z")}}
 
                 //var client2 = new MongoClient(new MongoClientSettings
                 //{
@@ -22,23 +23,22 @@ namespace MongoDB
                 //    ServerSelectionTimeout = TimeSpan.FromSeconds(1),
                 //});
 
+                string connectionString = "mongodb://127.0.0.1:27017?ServerSelectionTimeout=1";
                 var client = new MongoClient(connectionString);
                 var database = client.GetDatabase("greenfamily");
                 var collection = database.GetCollection<ExceptionLog>("OperationInfoLog");
 
-                //var find1 = collection.Find(item => item.Name == "北京").FirstOrDefault();
+                //单个查询
+                var find1 = collection.Find(item => item.FunctionName == "abcd").FirstOrDefault();
 
+                //集合查询
+                var list = collection.Find(item => item.CreateDate > DateTime.Now).ToList();
+
+                //分页查询
+                var list2 = collection.AsQueryable().Where(c => c.CreateDate > DateTime.Now).Skip(10).Take(10).ToList();
+
+                //删除
                 var result = collection.DeleteMany(c => c.CreateDate < DateTime.Parse("2017-7-30 00:00:00"));
-
-                //var list = collection.Find(item => item.CreateDate > DateTime.Now.AddDays(-1)).ToList();
-
-                //分页
-                //var list2 = collection.AsQueryable().Where(c => c.CreateDate > DateTime.Now.AddDays(-2)).Skip(10).Take(10).ToList();
-
-                //richTextBox1.Text = list2.ToJson();
-                //count = list2.Count;
-
-
                 long count = result.DeletedCount;
 
             }
