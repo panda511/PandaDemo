0d5ba7add28831f8e39ce563b319866fb5171722
sunjian <mayn@DESKTOP-Q08CDEK>
2018-05-14 19:07:34 +0800

    zzz

--- PandaDemo/WebSuperSocketServer/Program.cs
+++ PandaDemo/WebSuperSocketServer/Program.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using SuperSocket.SocketBase;
-using SuperWebSocket;
+using SuperSocket.SocketBase.Config;
+using SuperSocket.WebSocket;
 
 namespace WebSuperSocketServer
 {
@@ -12,38 +14,108 @@ namespace WebSuperSocketServer
     {
         static void Main(string[] args)
         {
-            var server = new WebSocketServer();
+            var server = new MyWebSocketServer();
 
             server.NewSessionConnected += ServerNewSessionConnected;
             server.NewMessageReceived += ServerNewMessageRecevied;
             server.SessionClosed += ServerSessionClosed;
 
+            server.NewDataReceived += (session, bytes) =>
+            {
+                //接收到新的二进制消息
+            };
+
+            Timer timer = new Timer((data) =>
+            {
+                //对当前已连接的符合条件的会话进行广播
+                var sessions = server.GetSessions(c => c.MyCustomerId == "1");
+                if (sessions != null)
+                {
+                    foreach (var session in sessions)
+                    {
+                        session.Send(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss"));
+                    }
+                }
+
+            }, null, 10000, 10000);
+
+          
             try
             {
-                server.Setup("127.0.0.1", 4141);
-                server.Start();
+                var listenerConfig1 = new ListenerConfig()
+                {
+                    Ip = "127.0.0.1",
+                    Port = 4141
+                };
+
+                var listenerConfig2 = new ListenerConfig()
+                {
+                    Ip = "127.0.0.1",
+                    Port = 4142
+                };
+
+                var list = new List<ListenerConfig>
+                {
+                    listenerConfig1,
+                    listenerConfig2
+                };
+
+                var serverConfig = new ServerConfig
+                {
+                    //Ip = "127.0.0.1",
+                    //Port = 4141,
+                    Listeners = list,
+                    //Name = "TestName",
+                    //ServerType = "",
+                    MaxConnectionNumber = 311 //最大连接数，默认100
+                };
+
+                //bool s1 = server.Setup("127.0.0.1", 4141); //对要侦听的IP及端口进行设置
+                bool s1 = server.Setup(serverConfig);
+                bool s2 = server.Start(); //启动侦听
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
+                Console.WriteLine("异常:" + ex.Message);
             }
             Console.ReadKey();
+
+            server.Stop();
         }
 
-        private static void ServerSessionClosed(WebSocketSession session, CloseReason value)
+        static int i = 1;
+
+        public static void ServerNewSessionConnected(MyWebSocketSession session)
         {
-            Console.WriteLine(session.Origin);
+            //Console.WriteLine("有新的连接:" + session.Origin);
+            Console.WriteLine("有新的连接:" + (i++));
         }
 
-        public static void ServerNewMessageRecevied(WebSocketSession session, string value)
+        public static void ServerNewMessageRecevied(MyWebSocketSession session, string value)
         {
-            Console.WriteLine(value);
-            session.Send("已收到：" + value);
+            Console.WriteLine("接收到新的文本消息:" + value);
+            session.Send("当前SessionID：" + session.SessionID);
+
+            session.MyCustomerId = value;
         }
 
-        public static void ServerNewSessionConnected(WebSocketSession session)
+
+        private static void ServerSessionClosed(MyWebSocketSession session, CloseReason value)
         {
-            Console.WriteLine(session.Origin);
+            Console.WriteLine("有断开的连接:" + value.ToString() + "     " + session.MyCustomerId);
         }
+
+    }
+
+
+
+    class MyWebSocketSession : WebSocketSession<MyWebSocketSession>
+    {
+        public string MyCustomerId { get; internal set; }
+    }
+
+    class MyWebSocketServer : WebSocketServer<MyWebSocketSession>
+    {
+
     }
 }
